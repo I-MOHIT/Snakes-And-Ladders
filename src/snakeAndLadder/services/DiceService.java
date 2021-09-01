45315efb22bb541bd052e7fee3646189dcbfaf15
I-MOHIT <mohitchugh180919@gmail.com>
2021-09-02 01:03:52 +0530

    Delete src/snakeAndLadder directory

--- src/snakeAndLadder/services/DiceService.java
+++
@@ -1,9 +0,0 @@
-package snakeAndLadder.services;
-
-public class DiceService {
-	public static int roll(int d) {
-		int min=1*d;
-		int max=6*d;
-		return (int) Math.floor(Math.random()*(max-min+1)+min);
-	}
-}
